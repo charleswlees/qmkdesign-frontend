47c3ddcd2ac021ab9d097323de68528513b35078
charleswlees <contact@charleswlees.com>
2025-07-02 11:22:08 -0400

    Adding vite context for env variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,3 +2,11 @@ declare module "*.css" {
   const content: string;
   export default content;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_GOOGLE_AUTH_CLIENT_ID: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
