d50b7c4e0940aac87312569fbe5567467d0d72a7
charleswlees <contact@charleswlees.com>
2025-08-05 23:38:27 -0400

    Uploading to S3 via workflow

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,5 @@
 FROM public.ecr.aws/docker/library/node:18-alpine
 
-COPY --from=public.ecr.aws/awsguru/aws-lambda-adapter:0.8.4 /lambda-adapter /opt/extensions/lambda-adapter
-
-ENV AWS_LWA_INVOKE_MODE=response_stream
-
 WORKDIR /app
 
 COPY package*.json ./
@@ -14,6 +10,3 @@ COPY . .
 
 RUN npm run build
 
-EXPOSE 5173
-
-CMD [ "npm", "run", "preview"]
--- terraform/resources.tf
+++ terraform/resources.tf
@@ -1,54 +1,144 @@
-resource "aws_ecr_repository" "qmkdesign-frontend" {
-  provider = aws.p1
-  name = "qmkdesign-frontend"
+# Charlie Lees
+# Based on online resource for setting up s3 and cloudwatch, see README for citation
+
+resource "aws_s3_bucket" "s3-static-website" {
+  bucket = "qmkdesign-frontend-bucket"
+  tags = {
+    Name = "S3 Bucket"
+  }
 }
 
-resource "aws_ecr_lifecycle_policy" "cleanup" {
-  provider   = aws.p1
-  repository = aws_ecr_repository.qmkdesign-frontend.name
+resource "aws_s3_bucket_public_access_block" "static_site_bucket_public_access" {
+  bucket = aws_s3_bucket.s3-static-website.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_website_configuration" "static_site_bucket_website_config" {
+  bucket = aws_s3_bucket.s3-static-website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+# S3 bucket policy to allow CloudFront access
+resource "aws_s3_bucket_policy" "static_site_bucket_policy" {
+  bucket = aws_s3_bucket.s3-static-website.id
 
   policy = jsonencode({
-    rules = [
+    Version = "2012-10-17"
+    Statement = [
       {
-        rulePriority = 1
-        description  = "Only keep 2 images in ECR"
-        selection = {
-          tagStatus   = "any"
-          countType   = "imageCountMoreThan"
-          countNumber = 2
+        Sid    = "AllowCloudFrontServicePrincipalReadOnly"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
         }
-        action = {
-          type = "expire"
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.s3-static-website.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.cf-dist.arn
+          }
         }
       }
     ]
   })
 }
 
-resource "aws_lambda_function" "frontend_lambda" {
-  provider      = aws.p1
-  function_name = "frontend_api"
-  role          = aws_iam_role.lambda_role.arn  
-  
-  package_type = "Image"
-  image_uri    = "${aws_ecr_repository.qmkdesign-frontend.repository_url}:latest"  
-  
-  timeout     = 30
-  memory_size = 128
-  
-  depends_on = [
-    aws_iam_role_policy_attachment.lambda_basic,
-    aws_iam_role_policy.lambda_policy
-  ]
+resource "aws_s3_object" "hosting_bucket_files" {
+  bucket = aws_s3_bucket.s3-static-website.id
+
+  for_each = module.template_files.files
+
+  key          = each.key
+  content_type = each.value.content_type
+
+  source  = each.value.source_path
+  content = each.value.content
+
+  etag = each.value.digests.md5
 }
 
-
-resource "aws_lambda_function_url" "api_url" {
-  provider           = aws.p1
-  function_name      = aws_lambda_function.frontend_lambda.function_name
-  authorization_type = "NONE"  
+resource "aws_cloudfront_origin_access_control" "cf-s3-oac" {
+  name                              = "CloudFront S3 OAC"
+  description                       = "CloudFront S3 OAC"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
 }
 
-output "function_url" {
-  value = aws_lambda_function_url.api_url.function_url
+resource "aws_cloudfront_distribution" "cf-dist" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name              = aws_s3_bucket.s3-static-website.bucket_regional_domain_name
+    origin_id                = aws_s3_bucket.s3-static-website.id
+    origin_access_control_id = aws_cloudfront_origin_access_control.cf-s3-oac.id
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.s3-static-website.id
+    
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_All"
+
+  custom_error_response {
+    error_code         = 403
+    response_page_path = "/index.html"
+    response_code      = 200
+  }
+
+  custom_error_response {
+    error_code         = 404
+    response_page_path = "/index.html"
+    response_code      = 200
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Name = "qmk-frontend-cloudfront"
+  }
+}
+
+output "cloudfront_distribution_url" {
+  value       = "https://${aws_cloudfront_distribution.cf-dist.domain_name}"
+  description = "The URL of the CloudFront distribution"
+}
+
+output "s3_bucket_name" {
+  value       = aws_s3_bucket.s3-static-website.id
+  description = "The name of the S3 bucket"
 }
